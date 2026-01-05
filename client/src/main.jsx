2ffd5693675867e45c6b8746343db04d47f9a00b
SriteXcode <riteshgangster420@gmail.com>
2026-01-05 11:56:45 +0530

    added backend in frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
+
+// Set default base URL for axios
+if (import.meta.env.VITE_API_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+}
+
 import "./i18n/i18n";
 import LanguageProvider from "./contexts/LanguageContext";
 import { AuthProvider } from "./contexts/AuthContext";
